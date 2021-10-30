9a04dc88e10eba545b8de5ae44c173e9617dccd3
Magferat <jmagferat@gmail.com>
2021-10-31 05:38:42 +0600

    server setup done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+
 const cors = require('cors')
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
@@ -22,6 +23,7 @@ async function run() {
         await client.connect();
         const database = client.db("foodKang");
         const itemsCollection = database.collection("items");
+        const orderCollection = database.collection("orders");
 
         // Post API 
         app.post('/items', async (req, res) => {
@@ -44,6 +46,62 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const item = await itemsCollection.findOne(query);
             res.json(item);
+        })
+        // Post Orders Api 
+        app.post('/orders', async (req, res) => {
+            const orderItem = req.body;
+
+            result = await orderCollection.insertOne(orderItem);
+            console.log(orderItem);
+            res.json(result);
+
+        })
+        // Get ALl Orders API
+        app.get('/orders', async (req, res) => {
+            const cursor = orderCollection.find({});
+            const order = await cursor.toArray();
+            res.send(order);
+        });
+        app.get('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const item = await orderCollection.findOne(query);
+            res.send(item);
+        })
+        // Get Specific User's Order 
+        app.get('/myOrders/:email', async (req, res) => {
+            const email = req.params.email;
+            const cursor = orderCollection.find({ email: email });
+            const order = await cursor.toArray();
+            res.send(order);
+            console.log(email)
+        })
+
+        //-------delete -------
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            console.log('Order Deleted', id)
+            res.json(result);
+
+        })
+        //   UPDATE API
+        app.put('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            // const updatedStatus = req.body;
+            // const options = { upsert: true };
+            const query = { _id: ObjectId(id) };
+            // const item = await orderCollection.findOne(query);
+            const update = {
+                $set: {
+                    status: "Approved"
+                }
+
+            };
+            const result = await orderCollection.updateOne(query, update)
+            console.log('updating', id)
+            res.json(result)
         })
     }
     finally {
